9579bb3b2ef8c827609e3fb3ba130c3015912314
dhruvparmar2707 <dhruvparmar149@gmail.com>
2025-10-07 19:09:35 +0530

    Add files via upload

---
+++ accept 5 values.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5];
+    printf("Enter 5 values:\n");
+    for(int i = 0; i < 5; i++)
+        scanf("%d", &arr[i]);
+    
+    printf("\nYou entered:\n");
+    for(int i = 0; i < 5; i++)
+        printf("%d ", arr[i]);
+    return 0;
+}
